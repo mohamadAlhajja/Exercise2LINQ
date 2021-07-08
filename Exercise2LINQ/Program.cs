24f14bfed2dd1db55c4287a0521e7862ff0a42e6
Mohamad Bilal Alhajjah <eng.mohamadalhajjah@outlook.com>
2021-07-08 15:33:30 +0300

    Add project files.

---
+++ Exercise2LINQ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise2LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> test = new List<string> { "he", "she", "z" };
+            Console.WriteLine($"the last word is {GetTheLastWord(test)}");
+
+
+
+        }
+        public static string GetTheLastWord(IEnumerable<string> words)
+        {
+            IEnumerable<string> haveE = words.Select(str => str).Where(str => str.Contains("e"));
+            IEnumerable<string> sorted = haveE.OrderBy(str => str);
+            string lastWord = sorted.LastOrDefault();
+
+            return lastWord;
+        }
+
+    }
+}
